be72a1496a171aad87ca241840db7d98f4054a4f
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-01-22 11:42:47 -0500

    Code formatting

--- routes/view/owner-project-list.js
+++ routes/view/owner-project-list.js
@@ -1,31 +1,31 @@
 var request = require('superagent');
 
 module.exports = function (req, res, next) {
-  var owner = req.params.owner
+  var owner = req.params.owner;
 
   request
     .get('https://api.github.com/users/' + owner + '/gists')
     .end(function (err, result) {
       if (err) {
-        next()
-        return
+        next();
+        return;
       }
 
       res.render('owner', {
         owner: owner,
         gists: result.body.filter(function (gist) {
-          var isValid = false
+          var isValid = false;
           req.app.locals.validFiles.forEach(function (file) {
-            if (gist.files[file]) isValid = true
-          })
-          return isValid
+            if (gist.files[file]) isValid = true;
+          });
+          return isValid;
         }).map(function (gist) {
           return {
             label: gist.description || gist.id,
             url: gist.id + '/',
             bgImage: gist.files['thumbnail.png'] ? gist.files['thumbnail.png'].raw_url : null
-          }
+          };
         })
-      })
-    })
-}
+      });
+    });
+};
