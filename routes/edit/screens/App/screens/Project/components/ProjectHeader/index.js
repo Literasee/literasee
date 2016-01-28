572e317cde46a5059382787115d69a9719b4dff5
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-01-28 11:49:39 -0500

    Style and render tweaks

--- routes/edit/screens/App/screens/Project/components/ProjectHeader/index.js
+++ routes/edit/screens/App/screens/Project/components/ProjectHeader/index.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router';
 
 import styles from './ProjectHeader.styl';
 
-export default ({ project }) => {
+export default ({ project, owner }) => {
   return (
     <div className={styles.container}>
       <Link className='mega-octicon octicon-chevron-left mr1'
         title='Back to your projects'
-        to={'/' + project.owner.login}>
+        to={'/' + owner}>
       </Link>
       <h3 className='mb0'>{project.id}</h3>
       <div className='description'>{project.description}</div>
--- routes/edit/screens/App/screens/Project/index.js
+++ routes/edit/screens/App/screens/Project/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import ProjectHeader from './components/ProjectHeader';
+
 import styles from './Project.styl';
 
 class Project extends Component {
   render () {
-    const { project } = this.props;
-    
+    const { project, params } = this.props;
+
     return (
       <div className={styles.container}>
-        <ProjectHeader project={project} />
-        <div style={{width: '100%', display: 'flex', flexGrow: 1}}>
+        <ProjectHeader project={project} owner={params.username} />
+        <div className={styles.contentArea}>
           <div style={{backgroundColor: 'lightgreen', width: '50%'}}>EDIT AREA</div>
           <div style={{backgroundColor: 'lightblue', width: '50%'}}>PREVIEW AREA</div>
         </div>
