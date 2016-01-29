7f11a357240deccf9e5706ce1157ef6c3037a559
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-01-29 15:43:38 -0500

    Fix header display

--- routes/edit/screens/App/screens/Project/components/ProjectHeader.js
+++ routes/edit/screens/App/screens/Project/components/ProjectHeader.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router';
 import styles from './ProjectHeader.styl';
 
 export default ({ project, owner }) => {
+  const [title, subTitle = ''] = (project.description || '').split(' | ');
+
   return (
     <div className={styles.container}>
       <Link className='mega-octicon octicon-chevron-left mr1'
         title='Back to your projects'
         to={'/' + owner}>
       </Link>
-      <h3 className='mb0'>{project.id}</h3>
-      <div className='description'>{project.description}</div>
+      <h3 className='mb0 mr1'>{title.trim() || project.id}</h3>
+      <div className={styles.subTitle}>{subTitle.trim()}</div>
     </div>
   )
 }
