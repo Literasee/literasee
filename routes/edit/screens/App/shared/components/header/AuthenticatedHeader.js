8bb51d98f9f5e0393d4d2622b914585308a95912
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-02-02 15:20:31 -0500

    Add logo SVG

--- routes/edit/screens/App/shared/components/header/AuthenticatedHeader.js
+++ routes/edit/screens/App/shared/components/header/AuthenticatedHeader.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Logo from './Logo';
 
-export default function ({username}) {
+export default function ({ username }) {
   return (
     <div className="row">
-      <h3 className="col-xs-4 col-md-6 txt-left mb0 mt0"><a href="#">Literasee</a></h3>
-      <nav role="navigation" className="col-xs-8 col-md-6">
+      <h3 className="col-xs-4 col-md-6 txt-left mb0 mt0">
+        <a href="/"><Logo scale="0.6" /></a>
+      </h3>
+      <nav
+        role="navigation"
+        className="col-xs-8 col-md-6"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-end'
+        }}>
         <ul className="list-inline list-unstyled cf fl-right">
           <li>Welcome <a href="#">{username}</a></li>
           <li>
--- routes/edit/screens/App/shared/components/header/DefaultHeader.js
+++ routes/edit/screens/App/shared/components/header/DefaultHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from './Logo';
 
 export default function () {
   return (
   <div className='row center-xs'>
     <div className='col-xs-12 col-md-8 mt2 mb2'>
-      <h1>Literasee</h1>
+      <h1><Logo /></h1>
       <p className='lead'>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
