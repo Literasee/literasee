bc5d148465911cdf4a6ceb90fc9a0f3754fc7298
dbetebenner <dbetebenner@gmail.com>
2016-02-09 15:41:47 -0500

    Adding logo and changing Lorem Ipsum

--- routes/edit/screens/App/shared/components/header/DefaultHeader.js
+++ routes/edit/screens/App/shared/components/header/DefaultHeader.js
@@ -7,8 +7,7 @@ export default function () {
     <div className='col-xs-12 col-md-8 mt2 mb2'>
       <h1><Logo /></h1>
       <p className='lead'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+       Create, Collaborate, Communicate
       </p>
       <a href={`https://github.com/login/oauth/authorize?client_id=${GH_CLIENT_ID}&scope=gist`} className='btn btn-primary btn-large btn-full-width'>Log in with Github</a>
     </div>
