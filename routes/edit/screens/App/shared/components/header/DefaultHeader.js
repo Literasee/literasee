a8a71d2fec51c8d081ccc4184d83afd079510e09
dbetebenner <dbetebenner@gmail.com>
2016-02-10 04:33:38 -0500

    Update splash page text

--- routes/edit/screens/App/shared/components/header/DefaultHeader.js
+++ routes/edit/screens/App/shared/components/header/DefaultHeader.js
@@ -7,7 +7,7 @@ export default function () {
     <div className='col-xs-12 col-md-8 mt2 mb2'>
       <h1><Logo /></h1>
       <p className='lead'>
-       Create | Collaborate | Communicate
+       Create • Collaborate • Communicate
       </p>
       <a href={`https://github.com/login/oauth/authorize?client_id=${GH_CLIENT_ID}&scope=gist`} className='btn btn-primary btn-large btn-full-width'>Log in with Github</a>
     </div>
