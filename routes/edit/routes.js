94b4be02f74fba4c946eec150c8804351a383152
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-02-24 10:28:06 -0500

    Redirect unauthenticated users to the home page

--- routes/edit/routes.js
+++ routes/edit/routes.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { Route, IndexRoute, Redirect } from 'react-router';
+import cookies from './config/cookies';
 
 import App from './screens/App';
 import UserContainer from './screens/App/screens/User/UserContainer';
 import AdminContainer from './screens/App/screens/Admin/AdminContainer';
 import ProjectContainer from './screens/App/screens/Project/ProjectContainer';
 
+const verifyToken = (nextState, replace) => {
+  if (!cookies.token) replace({pathname: '/'});
+}
+
 export default (
   <Route path='/' component={App}>
     <Route path=':username'>
-      <IndexRoute component={UserContainer} />
-      <Route path='admin' component={AdminContainer} />
+      <IndexRoute
+        onEnter={verifyToken} 
+        component={UserContainer} />
+
+      <Route
+        path='admin'
+        onEnter={verifyToken}
+        component={AdminContainer} />
+
       <Redirect from=':gistId' to=':gistId/report' />
-      <Route path=':gistId/:type' component={ProjectContainer} />
+      <Route
+        path=':gistId/:type'
+        onEnter={verifyToken}
+        component={ProjectContainer} />
     </Route>
   </Route>
 )
