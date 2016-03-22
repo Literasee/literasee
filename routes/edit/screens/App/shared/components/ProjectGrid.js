1034ed8acb3b3f3c200a5d683c1c7e905f1531db
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-03-22 10:29:36 -0400

    Use regular links for non-authenticated users

--- routes/edit/screens/App/shared/components/ProjectGrid.js
+++ routes/edit/screens/App/shared/components/ProjectGrid.js
@@ -15,6 +15,7 @@ export default ({projects, username, createGist}) => {
                 let [ title, subTitle ] = (project.description || '').split('|');
                 if (!title) title = project.project;
                 let linkDest = `/${project.owner}/${project.project}`;
+                let linkElement;
                 if (username && username !== project.owner) {
                   linkDest = `/${username}${linkDest}`;
                 }
@@ -31,16 +32,29 @@ export default ({projects, username, createGist}) => {
                   }
 
                   linkDest = `${protocol}//${hostname}${linkDest}`;
-                }
 
-                return (
-                  <div key={linkDest} className='col-xs-12 col-md-4 col-lg-4'>
+                  linkElement = (
+                    <a className='panel txt-left panel-project scales'
+                      style={{backgroundImage: `url("${image}")`}}
+                      href={linkDest}>
+                      <h4 className='mb0'>{title}</h4>
+                      <p>{subTitle}</p>
+                    </a>
+                  );
+                } else {
+                  linkElement = (
                     <Link className='panel txt-left panel-project scales'
                       style={{backgroundImage: `url("${image}")`}}
                       to={linkDest}>
                       <h4 className='mb0'>{title}</h4>
                       <p>{subTitle}</p>
                     </Link>
+                  );
+                }
+
+                return (
+                  <div key={linkDest} className='col-xs-12 col-md-4 col-lg-4'>
+                    {linkElement}
                   </div>
                 )
               })
