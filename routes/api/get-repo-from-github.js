f8b3a6d54fe33895eccb5378bd182915601a76ce
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-03-23 14:22:51 -0400

    Revert detailed error reporting, didn't work

--- routes/api/get-repo-from-github.js
+++ routes/api/get-repo-from-github.js
@@ -61,7 +61,7 @@ module.exports = function (req, res, next) {
     if (err) {
       if (err && err.status === 304) return next();
 
-      return res.status(err.status).json({err, req});
+      return res.status(err.status).json(err);
     }
 
     const info = result.info.body;
