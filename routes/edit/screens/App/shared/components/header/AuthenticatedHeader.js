0ecc07df71a17560d6c39603ef4430836d49973e
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-05-18 10:38:16 -0400

    New styling for authenticated header

--- routes/edit/screens/App/shared/components/header/AuthenticatedHeader.js
+++ routes/edit/screens/App/shared/components/header/AuthenticatedHeader.js
@@ -8,33 +8,35 @@ export default function ({ user, username, onCreateProject }) {
   const src = user ? user.avatar_url + '&s=40' : '';
 
   return (
-    <div className={styles.container}>
-      <a href='/'><Logo scale='0.6' /></a>
-      <nav
-        role='navigation'
-        className='pure-menu pure-menu-horizontal'>
-        <ul className='list-inline list-unstyled cf fl-right'>
-          <li>
-            <button
-              type='button'
-              name='button'
-              className='btn btn-primary fl-right'
-              onClick={onCreateProject}>
-              Create a Project
-            </button>
-          </li>
-          <li className='pure-menu-item pure-menu-has-children pure-menu-allow-hover'>
-            <a href='#' className={styles.avatarLink + ' pure-menu-link'}>
-              <img src={src} width='40' height='40' />
-            </a>
-            <ul className='pure-menu-children'>
-              <li className='pure-menu-item'>
-                <a href='/logout' className='pure-menu-link'>Log out</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <header className={styles.container + ' container-fluid'}>
+      <div>
+        <button
+          type='button'
+          name='button'
+          className='btn btn-primary'
+          onClick={onCreateProject}>
+          Create a Project
+        </button>
+      </div>
+      <div><a href='/'><Logo scale='0.6' /></a></div>
+      <div>
+        <nav
+          role='navigation'
+          className='pure-menu pure-menu-horizontal'>
+          <ul className='list-inline list-unstyled cf fl-right'>
+            <li className='pure-menu-item pure-menu-has-children pure-menu-allow-hover'>
+              <a href='#' className={styles.avatarLink + ' pure-menu-link'}>
+                <img src={src} width='40' height='40' />
+              </a>
+              <ul className='pure-menu-children'>
+                <li className='pure-menu-item'>
+                  <a href='/logout' className='pure-menu-link'>Log out</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   )
 }
--- routes/edit/screens/App/shared/components/header/index.js
+++ routes/edit/screens/App/shared/components/header/index.js
@@ -3,16 +3,14 @@ import React, { Component } from 'react';
 import DefaultHeader from './DefaultHeader';
 import AuthenticatedHeader from './AuthenticatedHeader';
 
-export default function ({user, username, onCreateProject, oauthUrl}) {
+export default function ({ user, username, onCreateProject, oauthUrl }) {
 
   if (username) {
     return (
-      <header className='container-fluid'>
-        <AuthenticatedHeader
-          user={user}
-          username={username}
-          onCreateProject={onCreateProject} />
-      </header>
+      <AuthenticatedHeader
+        user={user}
+        username={username}
+        onCreateProject={onCreateProject} />
     )
   }
 
