db86990deca2ccae6e614d3ecc1c1eaa72db4137
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-05-19 12:58:52 -0400

    Toolbar layout and styling. Not yet functional.

--- routes/edit/screens/App/screens/Project/components/Toolbar.js
+++ routes/edit/screens/App/screens/Project/components/Toolbar.js
@@ -6,7 +6,20 @@ class Toolbar extends Component {
   render () {
     return (
       <div className={styles.container}>
-        TOOLBAR
+        <div className={styles.buttonGroup}>
+          <button><img src='/public/img/toolbar/headings.png' /></button>
+          <button><img src='/public/img/toolbar/B.png' /></button>
+          <button><img src='/public/img/toolbar/i.png' /></button>
+        </div>
+        <div className={styles.buttonGroup}>
+          <button><img src='/public/img/toolbar/quote.png' /></button>
+          <button><img src='/public/img/toolbar/code.png' /></button>
+          <button><img src='/public/img/toolbar/link.png' /></button>
+        </div>
+        <div className={styles.buttonGroup}>
+          <button><img src='/public/img/toolbar/list1.png' /></button>
+          <button><img src='/public/img/toolbar/list2.png' /></button>
+        </div>
       </div>
     )
   }
