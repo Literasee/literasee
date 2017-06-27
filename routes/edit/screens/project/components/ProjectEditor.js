2c22840f5df54f4a1607cb3607774fd8a7e0515e
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-06-27 15:20:48 -0400

    Functional stateless components FTW

--- routes/edit/screens/project/components/ProjectEditor.js
+++ routes/edit/screens/project/components/ProjectEditor.js
@@ -6,42 +6,37 @@ import ProjectPreview from './ProjectPreview'
 
 import styles from './ProjectEditor.styl'
 
-class ProjectEditor extends Component {
-  render() {
-    const { params, project, code, onCodeChanged, onCancelChanges, onSaveChanges } = this.props
-    const mode = params.mode || 'edit'
+export default ({ params, project, code, onCodeChanged, onCancelChanges, onSaveChanges }) => {
+  const mode = params.mode || 'edit'
 
-    return (
-      <div className={styles.container}>
-        <Toolbar params={params} />
+  return (
+    <div className={styles.container}>
+      <Toolbar params={params} />
+      <div
+        style={{
+          display: 'flex',
+          flex: 1,
+        }}
+      >
         <div
           style={{
-            display: 'flex',
-            flex: 1,
+            flex: mode === 'edit' ? 1 : 0,
+            backgroundColor: 'white',
+            padding: '1rem',
           }}
         >
-          <div
-            style={{
-              flex: mode === 'edit' ? 1 : 0,
-              backgroundColor: 'white',
-              padding: '1rem',
-            }}
-          >
-            <CodeEditor code={code} onCodeChange={onCodeChanged} onSave={onSaveChanges} />
-          </div>
-          <ProjectPreview etag={project.etag} mode={mode} params={params} />
+          <CodeEditor code={code} onCodeChange={onCodeChanged} onSave={onSaveChanges} />
         </div>
-        <div className={styles.buttonContainer}>
-          <button onClick={onCancelChanges} type="button" name="button" className="btn">
-            Cancel
-          </button>
-          <button onClick={onSaveChanges} type="button" name="button" className="btn btn-primary">
-            Save
-          </button>
-        </div>
+        <ProjectPreview etag={project.etag} mode={mode} params={params} />
       </div>
-    )
-  }
+      <div className={styles.buttonContainer}>
+        <button onClick={onCancelChanges} type="button" name="button" className="btn">
+          Cancel
+        </button>
+        <button onClick={onSaveChanges} type="button" name="button" className="btn btn-primary">
+          Save
+        </button>
+      </div>
+    </div>
+  )
 }
-
-export default ProjectEditor
