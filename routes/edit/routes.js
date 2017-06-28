7621beb2fb5b23c0d1bef837842f416817c86a69
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-06-28 10:57:10 -0400

    Delete unused code

--- routes/edit/routes.js
+++ routes/edit/routes.js
@@ -8,33 +8,6 @@ import Homepage from './screens/homepage'
 import UserDashboard from './screens/userDashboard'
 import ProjectScreen from './screens/project'
 
-const verifyToken = (nextState, replace) => {
-  const { hostname, pathname } = document.location
-
-  // you cannot view another user's editor
-  if (hostname.indexOf('edit') === 0) {
-    if (!initialState.username || pathname.indexOf(initialState.username) < 0) {
-      replace({ pathname: '/' })
-    }
-  }
-}
-
-const verifyProjectType = (nextState, replace) => {
-  const { hostname } = document.location
-
-  // only authenticated users can use the editor
-  if (hostname.indexOf('edit') === 0 && !initialState.username) {
-    return replace({ pathname: '/' })
-  }
-
-  let { params: { mode }, location: { pathname } } = nextState
-  if (pathname.substr(-1) !== '/') pathname += '/'
-
-  // direct to report editing by default
-  mode = ['edit', 'preview'].indexOf(mode) < 0 ? 'edit' : null
-  if (mode) replace({ pathname: pathname + mode })
-}
-
 export default () =>
   <Router>
     <div className="wrapper">
@@ -50,20 +23,3 @@ export default () =>
       <Footer />
     </div>
   </Router>
-
-// <IndexRoute component={UserContainer} />
-//     <Route path=":username">
-//       <IndexRoute onEnter={verifyToken} component={UserContainer} />
-
-//       <Route
-//         path=":project(/:mode)"
-//         onEnter={verifyProjectType}
-//         component={ProjectContainer}
-//       />
-
-//       <Route
-//         path=":owner/:project(/:mode)"
-//         onEnter={verifyProjectType}
-//         component={ProjectContainer}
-//       />
-//     </Route>
