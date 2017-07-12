7ef6fd66ea214c0eb13afa8ec3cfeda572beb80f
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-07-12 12:06:01 -0400

    Flatten promise chain for saving, catch errors

--- routes/api/save-project.js
+++ routes/api/save-project.js
@@ -4,15 +4,24 @@ const data = require('../../persistence')
 module.exports = function(req, res) {
   const project = req.body
 
-  requests.createTree(req, project).then(tree => {
-    requests.createCommit(req, project, tree.body).then(commit => {
-      project.lastCommit = commit.body
-      requests.updateRef(req, project, commit.body).then(() => {
-        requests.getRepoInfo(req).then(info => {
-          project.etag = info.headers.etag
-          data.saveProject(project).then(p => res.json(p))
-        })
-      })
+  requests
+    .createTree(req, project)
+    .then(tree => {
+      return requests.createCommit(req, project, tree.body)
     })
-  })
+    .then(commit => {
+      project.lastCommit = commit.body
+      return requests.updateRef(req, project, commit.body)
+    })
+    .then(() => {
+      return requests.getRepoInfo(req)
+    })
+    .then(info => {
+      project.etag = info.headers.etag
+      return data.saveProject(project)
+    })
+    .then(p => res.json(p))
+    .catch(e => {
+      res.status(500).json(e)
+    })
 }
