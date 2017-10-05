e302b41b54d37ad45817df1be47f4b1c12f575a8
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-10-05 15:54:24 -0400

    Careful now

--- routes/edit/screens/project/components/ProjectPreview.js
+++ routes/edit/screens/project/components/ProjectPreview.js
@@ -6,12 +6,18 @@ import styles from './ProjectPreview.styl'
 
 export default ({ params, isActive, code }) => {
   const { owner, username, project } = params
+  let ast, display
+
+  try {
+    ast = compile(code)
+    display = <IdyllDocument key={code} ast={ast} components={components} />
+  } catch (e) {
+    return <pre>{e.message}</pre>
+  }
 
   return (
     <div className={styles.container} style={{ flex: isActive ? 1 : 0 }}>
-      <div className={styles.previewWrapper}>
-        <IdyllDocument key={code} ast={compile(code)} components={components} />
-      </div>
+      <div className={styles.previewWrapper}>{display}</div>
     </div>
   )
 }
