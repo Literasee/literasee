20d4ae9edbe776a514f04e883e2820dd3b59327f
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-10-12 12:52:01 -0400

    Add generated, scoped CSS. Add dynamic application for in-app styling previews.

---
+++ routes/edit/screens/project/IdyllStyles.js
@@ -0,0 +1,22 @@
+// layouts
+import blog from '../../styles/layouts/blog.css'
+import centered from '../../styles/layouts/centered.css'
+
+// themes
+import github from '../../styles/themes/github.css'
+import idyll from '../../styles/themes/idyll.css'
+import none from '../../styles/themes/none.css'
+import tufte from '../../styles/themes/tufte.css'
+
+export const layouts = {
+  blog,
+  centered,
+  none,
+}
+
+export const themes = {
+  github,
+  idyll,
+  none,
+  tufte,
+}
