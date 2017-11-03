6bb7cf15e0e72a2e20890486c76506fd4b8a1525
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-11-03 14:02:59 -0400

    Serve assets requested by previews

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const { join } = require('path')
+const { existsSync } = require('fs')
 const express = require('express')
 const engines = require('consolidate')
 const favicon = require('serve-favicon')
@@ -6,11 +7,11 @@ const db = require('./persistence/db')
 
 const app = express()
 
-app.use(favicon(path.join(__dirname, 'public/favicon.ico')))
+app.use(favicon(join(__dirname, 'public/favicon.ico')))
 app.use(require('cookie-parser')())
 
 // configure views
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 app.engine('hbs', engines.handlebars)
 
@@ -36,9 +37,25 @@ app.use('/preview', require('./routes/preview'))
 app.use('/api', require('./routes/api'))
 app.use('/', require('./routes/edit'))
 
+// serve assets requested by previews
+app.use('/:owner/:name/:asset', (req, res, next) => {
+  const { owner, name, asset } = req.params
+  const dir = join(__dirname, 'tmp', owner, name)
+
+  if (!existsSync(dir)) {
+    return res.status(404).send('Project does not exist.')
+  }
+  if (!asset.includes('.')) {
+    return next()
+  }
+
+  res.setHeader('Cache-Control', 'no-cache')
+  res.sendFile(join(dir, asset))
+})
+
 // the React app is the fallback for all routes
 app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, 'routes', 'edit', 'index.html'))
+  res.sendFile(join(__dirname, 'routes', 'edit', 'index.html'))
 })
 
 module.exports = function(local, port) {
