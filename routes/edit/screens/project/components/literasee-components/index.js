1c8c745b30e9b178d985fc6549393119bf5cbe02
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-11-10 15:34:28 -0500

    Add a basic POC component

--- routes/edit/screens/project/components/literasee-components/index.js
+++ routes/edit/screens/project/components/literasee-components/index.js
@@ -1,7 +1,10 @@
+import React from 'react'
+import BasicLiteraseeComponent from './basic-literasee-component'
 import ComplaintsTable from './complaints-table'
 import LiteraseeLogo from './literasee-logo'
 
 export default {
+  BasicLiteraseeComponent,
   ComplaintsTable,
   LiteraseeLogo,
 }
